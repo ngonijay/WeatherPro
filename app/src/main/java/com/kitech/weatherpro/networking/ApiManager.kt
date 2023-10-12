32fb5ae31a33f8e14a6b759bae872e0630499012
Ngonidzashe.Jambo <ngonidzashe.jambo@ecocashholding.co.zw>
2023-10-12 13:43:51 +0200

    add retrofit api manager

---
+++ app/src/main/java/com/kitech/weatherpro/networking/ApiManager.kt
@@ -0,0 +1,35 @@
+package com.kitech.weatherpro.networking
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+object ApiManager {
+
+    private val gson: Gson = GsonBuilder()
+        .enableComplexMapKeySerialization()
+        .create()
+
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+
+    private val client = OkHttpClient.Builder()
+
+        .addInterceptor(loggingInterceptor)
+        .readTimeout(5, TimeUnit.MINUTES)
+        .connectTimeout(5, TimeUnit.MINUTES)
+        .build()
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl("api.openweathermap.org/data/2.5/")
+        .client(client)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+}
